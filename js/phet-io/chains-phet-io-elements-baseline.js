8039cf8f906e045aaf53e865525253071a44eb69
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 14:40:20 -0800

    recompute baseline, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/chains-phet-io-elements-baseline.js
+++ js/phet-io/chains-phet-io-elements-baseline.js
@@ -334,7 +334,7 @@ window.phet.phetio.phetioElementsBaseline =
     "chains.chainsScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
@@ -411,7 +411,7 @@ window.phet.phetio.phetioElementsBaseline =
     "chains.chainsScreen.view.resetAllButton.visibleProperty": {
       "phetioDocumentation": "Controls whether the Node will be visible (and interactive), see the NodeIO documentation for more details.",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
@@ -774,7 +774,7 @@ window.phet.phetio.phetioElementsBaseline =
     "chains.navigationBar.phetButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
