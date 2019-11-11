2a56a90ec6c125f65ee9c4d0380e7304526728a6
samreid <reids@colorado.edu>
2019-11-11 08:43:02 -0700

    Rename PhetioCapsule => PhetioSingleton, see https://github.com/phetsims/phet-io/issues/1567

--- js/phet-io/chains-phet-io-types.js
+++ js/phet-io/chains-phet-io-types.js
@@ -557,7 +557,7 @@ window.phet.phetio.phetioTypes = assert &&
       "supertype": "ObjectIO",
       "typeName": "PhetMenuIO"
     },
-    "PhetioCapsuleIO<DialogIO>": {
+    "PhetioSingletonIO<DialogIO>": {
       "documentation": "An array that sends notifications when its values have changed.",
       "events": [],
       "methodOrder": [],
@@ -566,7 +566,7 @@ window.phet.phetio.phetioTypes = assert &&
         "DialogIO"
       ],
       "supertype": "ObjectIO",
-      "typeName": "PhetioCapsuleIO<DialogIO>"
+      "typeName": "PhetioSingletonIO<DialogIO>"
     },
     "PhetioCommandProcessorIO": {
       "documentation": "Invokes PhET-iO API commands on a running simulation.",
