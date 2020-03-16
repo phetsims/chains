9efb6f2f846c1a4a15b536a24457da6d0efeaa65
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 14:42:11 -0800

    remove ResetAllButtonIO.js, https://github.com/phetsims/scenery-phet/issues/572

--- js/phet-io/chains-phet-io-elements-baseline.js
+++ js/phet-io/chains-phet-io-elements-baseline.js
@@ -402,7 +402,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "chains.chainsScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
