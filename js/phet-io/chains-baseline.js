84f0aaf35e5c5826ed38d28a280b1a9d1b28cc7d
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:16:30 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/chains-baseline.js
+++ js/phet-io/chains-baseline.js
@@ -27,19 +27,6 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "chains.chainsScreen.nameProperty": {
-      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
-      "phetioDynamicElement": false,
-      "phetioEventType": "MODEL",
-      "phetioFeatured": true,
-      "phetioHighFrequency": false,
-      "phetioIsArchetype": false,
-      "phetioPlayback": false,
-      "phetioReadOnly": false,
-      "phetioState": true,
-      "phetioStudioControl": true,
-      "phetioTypeName": "PropertyIO<NullableIO<StringIO>>"
-    },
     "chains.chainsScreen.view": {
       "phetioDocumentation": "",
       "phetioDynamicElement": false,
