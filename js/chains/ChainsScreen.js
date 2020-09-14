3d5344563512b24903741225369a16f4345fe9c8
pixelzoom <cmalley@pixelzoom.com>
2020-09-14 16:16:34 -0600

    convert chains to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/chains/ChainsScreen.js
+++ js/chains/ChainsScreen.js
@@ -7,23 +7,18 @@
  */
 
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import chains from '../chains.js';
 import ChainsView from './view/ChainsView.js';
 
-/**
- * @constructor
- */
-function ChainsScreen( tandem ) {
-
-  Screen.call( this,
-    function() { return {}; },
-    function( model ) { return new ChainsView( model, tandem.createTandem( 'view' ) ); },
-    { tandem: tandem }
-  );
+class ChainsScreen extends Screen {
+  constructor( tandem ) {
+    super(
+      function() { return {}; },
+      function( model ) { return new ChainsView( model, tandem.createTandem( 'view' ) ); },
+      { tandem: tandem }
+    );
+  }
 }
 
 chains.register( 'ChainsScreen', ChainsScreen );
-
-inherit( Screen, ChainsScreen );
 export default ChainsScreen;
