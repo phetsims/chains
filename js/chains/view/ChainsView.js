bf4c22033b36bcdac9731ffd76c3caa6a69a4e73
Sam Reid <reids@colorado.edu>
2021-10-01 10:41:38 -0600

    Revert "Convert chains to TypeScript, see https://github.com/phetsims/chipper/issues/1103"
    This reverts commit c5ec404d01acc01ead52a9819f3c1925244f2609.

--- js/chains/view/ChainsView.js
+++ js/chains/view/ChainsView.js
@@ -1,5 +1,5 @@
 // Copyright 2015-2021, University of Colorado Boulder
-// @ts-nocheck
+
 /**
  * View for the 'Chains' screen.
  *
@@ -28,9 +28,6 @@ const unitsNmString = sceneryPhetStrings.units_nm;
 
 // constants
 const FONT = new PhetFont( 36 );
-
-const x: number = 7;
-console.log( 'x = ' + x );
 
 class ChainsView extends ScreenView {
 
