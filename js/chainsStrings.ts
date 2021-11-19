254367a55307ceed9ab8ae9ab62c651a4e6047aa
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:12 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/chainsStrings.ts
@@ -0,0 +1,26 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import chains from './chains.js';
+
+type StringsType = {
+  'chains': {
+    'title': string
+  },
+  'plainString': string,
+  'multilineString': string,
+  'htmlString': string,
+  'patternString': string,
+  'namedPlaceholdersString': string,
+  'size': string
+};
+
+const chainsStrings = getStringModule( 'CHAINS' ) as StringsType;
+
+chains.register( 'chainsStrings', chainsStrings );
+
+export default chainsStrings;
