001dce78a0ffe2f59670961bf7eb4817d32353d8
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:42 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/chainsStrings.ts
+++ js/chainsStrings.ts
@@ -5,18 +5,26 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import chains from './chains.js';
 
 type StringsType = {
   'chains': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'plainString': string;
+  'plainStringProperty': TReadOnlyProperty<string>;
   'multilineString': string;
+  'multilineStringProperty': TReadOnlyProperty<string>;
   'htmlString': string;
+  'htmlStringProperty': TReadOnlyProperty<string>;
   'patternString': string;
+  'patternStringProperty': TReadOnlyProperty<string>;
   'namedPlaceholdersString': string;
+  'namedPlaceholdersStringProperty': TReadOnlyProperty<string>;
   'size': string;
+  'sizeProperty': TReadOnlyProperty<string>;
 };
 
 const chainsStrings = getStringModule( 'CHAINS' ) as StringsType;
