536d03cea926ff031a3a6834de2359c0e0971d47
Sam Reid <reids@colorado.edu>
2025-06-13 20:28:12 -0600

    grunt modulify, see https://github.com/phetsims/membrane-transport/issues/230

--- js/ChainsFluent.ts
+++ js/ChainsFluent.ts
@@ -19,22 +19,23 @@ import ChainsStrings from './ChainsStrings.js';
 // the build. So we need to only add actually used strings.
 const fluentKeyToStringPropertyMap = new Map();
 
-const addToMapIfDefined = ( key: string, sp: TReadOnlyProperty<string> | undefined ) => {
+const addToMapIfDefined = ( key: string, path: string ) => {
+  const sp = _.get( ChainsStrings, path );
   if ( sp ) {
     fluentKeyToStringPropertyMap.set( key, sp );
   }
 };
 
-addToMapIfDefined( 'chains_title', ChainsStrings?.["chains"]?.["titleStringProperty"] );
-addToMapIfDefined( 'plainString', ChainsStrings?.["plainStringStringProperty"] );
-addToMapIfDefined( 'multilineString', ChainsStrings?.["multilineStringStringProperty"] );
-addToMapIfDefined( 'htmlString', ChainsStrings?.["htmlStringStringProperty"] );
-addToMapIfDefined( 'size', ChainsStrings?.["sizeStringProperty"] );
-addToMapIfDefined( 'a11y_nested_constant', ChainsStrings?.["a11y"]?.["nested"]?.["constantStringProperty"] );
-addToMapIfDefined( 'a11y_nested_fluentReference', ChainsStrings?.["a11y"]?.["nested"]?.["fluentReferenceStringProperty"] );
-addToMapIfDefined( 'a11y_nested_fluentVariable', ChainsStrings?.["a11y"]?.["nested"]?.["fluentVariableStringProperty"] );
-addToMapIfDefined( 'a11y_nested_selector', ChainsStrings?.["a11y"]?.["nested"]?.["selectorStringProperty"] );
-addToMapIfDefined( 'a11y_nested_cascade', ChainsStrings?.["a11y"]?.["nested"]?.["cascadeStringProperty"] );
+addToMapIfDefined( 'chains_title', 'chains.titleStringProperty' );
+addToMapIfDefined( 'plainString', 'plainStringStringProperty' );
+addToMapIfDefined( 'multilineString', 'multilineStringStringProperty' );
+addToMapIfDefined( 'htmlString', 'htmlStringStringProperty' );
+addToMapIfDefined( 'size', 'sizeStringProperty' );
+addToMapIfDefined( 'a11y_nested_constant', 'a11y.nested.constantStringProperty' );
+addToMapIfDefined( 'a11y_nested_fluentReference', 'a11y.nested.fluentReferenceStringProperty' );
+addToMapIfDefined( 'a11y_nested_fluentVariable', 'a11y.nested.fluentVariableStringProperty' );
+addToMapIfDefined( 'a11y_nested_selector', 'a11y.nested.selectorStringProperty' );
+addToMapIfDefined( 'a11y_nested_cascade', 'a11y.nested.cascadeStringProperty' );
 
 // A function that creates contents for a new Fluent file, which will be needed if any string changes.
 const createFluentFile = (): string => {
@@ -49,18 +50,18 @@ const fluentSupport = new FluentContainer( createFluentFile, Array.from(fluentKe
 
 const ChainsFluent = {
   chains: {
-    titleStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'chains_title' )
+    titleStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'chains_title', _.get( ChainsStrings, 'chains.titleStringProperty' )  )
   },
-  plainStringStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'plainString' ),
-  multilineStringStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'multilineString' ),
-  htmlStringStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'htmlString' ),
-  patternStringStringProperty: ChainsStrings?.["patternStringStringProperty"],
-  namedPlaceholdersStringStringProperty: ChainsStrings?.["namedPlaceholdersStringStringProperty"],
-  sizeStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'size' ),
+  plainStringStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'plainString', _.get( ChainsStrings, 'plainStringStringProperty' )  ),
+  multilineStringStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'multilineString', _.get( ChainsStrings, 'multilineStringStringProperty' )  ),
+  htmlStringStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'htmlString', _.get( ChainsStrings, 'htmlStringStringProperty' )  ),
+  patternStringStringProperty: _.get( ChainsStrings, 'patternStringStringProperty' ),
+  namedPlaceholdersStringStringProperty: _.get( ChainsStrings, 'namedPlaceholdersStringStringProperty' ),
+  sizeStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'size', _.get( ChainsStrings, 'sizeStringProperty' )  ),
   a11y: {
     nested: {
-      constantStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'a11y_nested_constant' ),
-      fluentReferenceStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'a11y_nested_fluentReference' ),
+      constantStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'a11y_nested_constant', _.get( ChainsStrings, 'a11y.nested.constantStringProperty' )  ),
+      fluentReferenceStringProperty: new FluentConstant( fluentSupport.bundleProperty, 'a11y_nested_fluentReference', _.get( ChainsStrings, 'a11y.nested.fluentReferenceStringProperty' )  ),
       fluentVariable: new FluentPattern<{ variable: FluentVariable }>( fluentSupport.bundleProperty, 'a11y_nested_fluentVariable' ),
       selector: new FluentPattern<{ activityLevel: 'calm' | 'active' | 'activeAndPaused' | TReadOnlyProperty<'calm' | 'active' | 'activeAndPaused'> }>( fluentSupport.bundleProperty, 'a11y_nested_selector' ),
       cascade: new FluentPattern<{ activityLevel: 'calm' | 'active' | 'activeAndPaused' | TReadOnlyProperty<'calm' | 'active' | 'activeAndPaused'> }>( fluentSupport.bundleProperty, 'a11y_nested_cascade' )
