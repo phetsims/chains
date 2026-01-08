ca8d3bacd1fef8df7ece6f0eb3f14e7db0ace769
phet-dev <phetadmin@gmail.com>
2026-01-08 03:37:22 -0700

    update copyright dates from daily grunt work

--- js/ChainsFluent.ts
+++ js/ChainsFluent.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 // AUTOMATICALLY GENERATED – DO NOT EDIT.
 // Generated from chains-strings_en.yaml
 
